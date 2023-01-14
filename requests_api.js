72c20fb5cd2702c9a071754c302c6aca73b37af5
soni1541 <kurbatovasoni5@gmail.com>
2023-01-14 16:39:30 +0500

    Update requests_api.js

--- requests_api.js
+++ requests_api.js
@@ -1,23 +1,40 @@
 
                 
-// function appendData(data) {
-//     var product_container = document.getElementById('products');        
-//     for (var i = 0; i < data.length; i++) {
-//         var div = document.createElement("div");
-//         div.className = "product";
-//         div.innerHTML =  '<div class="product-info">'+
-//                             '<h1>' + data[i].title + '</h1>' +
-//                             '<h2>' + data[i].category + '</h2>' +
-//                         '</div>' +
-//                         '<div class="img-container"><img src="' + data[i].image + '"></div>' +
-//                         '<div class="product-info">'+
-//                             '<span class="price">' + "$" + data[i].price + '</span>'+
-//                             '<span class="rate">' + data[i].rating.rate + '</span>' +
-//                             '<a href="" class="button">В корзину</a>' +
-//                         '</div>';
-//         product_container.appendChild(div);
-//     }
-// }
+function appendData(data) {
+    var product_container = document.getElementById('products');        
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.className = "product";
+        div.innerHTML =  '<div class="product-info">'+
+                            '<h1>' + data[i].title + '</h1>' +
+                            '<h2>' + data[i].category + '</h2>' +
+                        '</div>' +
+                        '<div class="img-container"><img src="' + data[i].image + '"></div>' +
+                        '<div class="product-info">'+
+                            '<span class="price">' + "$" + data[i].price + '</span>'+
+                            '<span class="rate">' + data[i].rating.rate + '</span>' +
+                            '<a href="" class="button">В корзину</a>' +
+                        '</div>';
+        product_container.appendChild(div);
+    }
+}
+
+function append_item (item, cont)
+{
+    var div = document.createElement("div");
+    div.className = "product";
+    div.innerHTML =  '<div class="product-info">'+
+                        '<h1>' + item.title + '</h1>' +
+                        '<h2>' + item.category + '</h2>' +
+                    '</div>' +
+                    '<div class="img-container"><img src="' + item.image + '"></div>' +
+                    '<div class="product-info">'+
+                        '<span class="price">' + "$" + item.price + '</span>'+
+                        '<span class="rate">' + item.rating.rate + '</span>' +
+                        '<a href="" class="button">В корзину</a>' +
+                    '</div>';
+    cont.appendChild(div);
+}
 
 
 function appendCat(categories) {
@@ -59,33 +76,27 @@ document.addEventListener('DOMContentLoaded', function ()
     var el = document.getElementById("select");
     el.onchange = function ()
     {
-        var product_container = document.getElementById('products');
-        var div = document.getElementsByClassName('product');
         
-        if(cate_now == 'All')
-        {
-
-        }
+        var product_container = document.getElementById('products');       
+        product_container.parentNode.removeChild(product_container);
+        var products_container = document.createElement("div");
+        products_container.id = "products";
+        document.body.appendChild(products_container);
+        
         for(var i = 0; i < prod.length; i++)
         {
-            var cate_now = prod[i].category;
-
-            if(cate_now == el.value)
+            if(el.value == 'All')
             {
-                var div = document.createElement("div");
-                div.className = "product";
-                div.innerHTML =  '<div class="product-info">'+
-                                    '<h1>' + prod[i].title + '</h1>' +
-                                    '<h2>' + prod[i].category + '</h2>' +
-                                '</div>' +
-                                '<div class="img-container"><img src="' + prod[i].image + '"></div>' +
-                                '<div class="product-info">'+
-                                    '<span class="price">' + "$" + prod[i].price + '</span>'+
-                                    '<span class="rate">' + prod[i].rating.rate + '</span>' +
-                                    '<a href="" class="button">В корзину</a>' +
-                                '</div>';
-                product_container.appendChild(div);
+                append_item(prod[i], products_container);
+            }
+            else
+            {
+                var cate_now = prod[i].category;
 
+                if(cate_now == el.value)
+                {
+                    append_item(prod[i], products_container);
+                }
             }
         }
     }
